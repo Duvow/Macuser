1ba5a7df40bd21e6d8b650a538bcbbbad24d00e8
AnselmoCabrera <cabreraanselmo@gmail.com>
2025-11-06 17:28:28 +0100

    se me ha olvidado guardar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Home } from './pages/home/home';
+import { Machinery } from './pages/machinery/machinery';
+import { UserDetail } from './pages/user-detail/user-detail';
+import { MachineryDetail } from './pages/machinery-detail/machinery-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'home', component: Home},
+    { path: 'machinery', component: MachineryDetail},
+    { path: 'machinery/:id', component: Machinery},
+    { path: 'user/:id' , component: UserDetail}
+];
