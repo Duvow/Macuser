02a3853e6f9d1876ef7bc0c2425fdf76dbfc0dd9
jlep <espejeljoseluis57@gmail.com>
2025-11-06 18:53:54 +0100

    machinery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Home } from './pages/home/home';
-import { Machinery } from './pages/machinery/machinery';
+import { MachineryPage } from './pages/machinery/machinery';
 import { UserDetail } from './pages/user-detail/user-detail';
 import { MachineryDetail } from './pages/machinery-detail/machinery-detail';
 
@@ -10,7 +10,7 @@ export const routes: Routes = [
     { path: 'login', component: Login },
     { path: 'register', component: Register },
     { path: 'home', component: Home},
-    { path: 'machinery', component: MachineryDetail},
-    { path: 'machinery/:id', component: Machinery},
+    { path: 'machinery', component: MachineryPage},
+    { path: 'machinery/:id', component: MachineryDetail},
     { path: 'user/:id' , component: UserDetail}
 ];
--- src/app/model/machinery.ts
+++ src/app/model/machinery.ts
@@ -1,2 +1,16 @@
 export class Machinery {
+
+    id: number;
+    name: string;
+    year: number;
+    color: string;
+    pantone_value: string;
+
+    constructor(id: number, name: string, year: number, color: string, pantone_value: string) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.color = color;
+        this.pantone_value = pantone_value;
+    }
 }
