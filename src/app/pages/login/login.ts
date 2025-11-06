7005fa3e046393b709dfb216330871fdcda38e1d
Madssen44 <kevinjgaleano@gmail.com>
2025-11-06 19:35:21 +0100

    Acabo de validar login y redirijo a home

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { Login } from '../../model/login';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { LoginServices } from '../../services/login-services';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule,RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class LoginPage {
+  constructor(private router: Router, private loginservice:LoginServices) { }
   login: Login = new Login('', '', '');
+  
   verifyLogin(){
+    this.login.email = "test@test.com"
+    this.loginservice.login(this.login).subscribe({
+      next: (response) => {
+        console.log('Total de páginas', response);
+        this.router.navigate(["/home"])
+      },
+      error: (err) => {
+        console.error('Error al cargar los usuarios', err);
+      }
+    });
     return this.login
   }
 }
--- src/app/services/login-services.ts
+++ src/app/services/login-services.ts
@@ -1,8 +1,19 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Login } from '../model/login';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginServices {
-  
+  private baseUrl = 'https://reqres.in/api/';
+  private headers = new HttpHeaders()
+    .set('x-api-key', 'reqres-free-v1');
+
+  constructor(private http: HttpClient) { }
+
+  login(login:Login): Observable<any> {
+    return this.http.post<any>(this.baseUrl + 'login', login, {headers: this.headers });
+  }
 }
