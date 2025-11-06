0d37e67d1bac50a0ca36d5ec8b127d62972bef4f
Madssen44 <kevinjgaleano@gmail.com>
2025-11-06 18:57:57 +0100

    Modifico login.ts, maqueto login.html

--- src/app/model/login.ts
+++ src/app/model/login.ts
@@ -1,11 +1,11 @@
 export class Login {
     username: string;
-    email: string;
+    email:string;
     password: string;
 
-    constructor(email: string, username: string, password: string) {
+    constructor (username:string, email:string,password:string){
+        this.username=  username;
         this.email = email;
-        this.username = username;
-        this.password = password;
+        this.password= password;  
     }
 }
