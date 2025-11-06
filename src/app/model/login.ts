f0fd920a628da493a218e6ceffa3e8172029cc7b
Madssen44 <kevinjgaleano@gmail.com>
2025-11-06 18:10:18 +0100

    añado atributos al login y formateo la pagina login

--- src/app/model/login.ts
+++ src/app/model/login.ts
@@ -1,2 +1,11 @@
 export class Login {
+    username: string;
+    email: string;
+    password: string;
+
+    constructor(email: string, username: string, password: string) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 }
