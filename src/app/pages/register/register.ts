8400f09b41c791b459c9c321830b96505c06bda5
AnselmoCabrera <cabreraanselmo@gmail.com>
2025-11-06 19:51:24 +0100

    Cambio Register

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { LoginServices } from '../../services/login-services';
+import { Login } from '../../model/login';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  login: Login = new Login('', '', '');
+   
 
+    constructor(private registerServices : LoginServices, private router: Router){}
+
+    register(){
+      console.log(this.login)
+
+    }
+
+    gobacktologin(){
+      this.router.navigate(["/login"])
+    }
 }
